1e1c151cb47cf83b25f22b50eb5faaee5a9b13c7
dkgulati2611 <dipesh.ug20@nsut.ac.in>
2023-05-16 10:37:25 +0530

    TodoList

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Modal from 'react-modal';
 
 function App() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [todos, setTodos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleAddTodo = (todoText) => {
+    setTodos([...todos, todoText]);
+    setModalIsOpen(false);
+  };
+
+  const handleDeleteTodo = (index) => {
+    const newTodos = todos.filter((todo, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  const handleSearchInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredTodos = todos.filter((todo) =>
+    todo.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <div className="heading">
+        <h1>TODO List</h1>
+      </div>
+
+      <input type="text" placeholder="Search..." onChange={handleSearchInputChange} />
+
+      <button onClick={() => setModalIsOpen(true)}>Add a todo</button>
+
+      <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)} className="container">
+        <h2>Add a todo</h2>
+
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            const todoText = event.target.todoText.value;
+            handleAddTodo(todoText);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <input type="text" name="todoText" placeholder="Enter a new todo" />
+          <button type="submit">Submit</button>
+        </form>
+      </Modal>
+
+      <ul>
+        {filteredTodos.map((todo, index) => (
+          <li key={index}>
+            {todo} <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
